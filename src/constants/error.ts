55faa323f25708f66a886a2e2f356ab7cedc8041
tumeraltunbas <tumer@adsby.co>
2025-05-19 18:52:49 +0300

    chore: initialize project with boilerplate

---
+++ src/constants/error.ts
@@ -0,0 +1,17 @@
+export const ERROR_CODES = {
+    processFailureError: 'process_failure_error',
+    userAlreadyExists: 'user_already_exists',
+    validationError: 'validation_error',
+    invalidCredentials: 'invalid_credentials',
+    inactiveUser: 'inactive_user',
+    authorizationError: 'authorization_error',
+};
+
+export const ERROR_MESSAGES = {
+    process_failure_error: 'Process failure error.',
+    user_already_exists: 'User already exists.',
+    validation_error: 'Validation error.',
+    invalid_credentials: 'Invalid credentials.',
+    inactive_user: 'Inactive user.',
+    authorization_error: 'Authorization error.',
+};
---
+++ src/utils/error.ts
@@ -0,0 +1,33 @@
+import { HttpStatus } from '@nestjs/common';
+import { ERROR_CODES, ERROR_MESSAGES } from '../constants/error';
+import {
+    BaseError,
+    ErrorResponseBody,
+    ValidationError,
+} from '../infrastructure/error/error';
+
+export function errorCodeToMessage(errorCode: string): string {
+    return ERROR_MESSAGES[errorCode] ?? ERROR_MESSAGES.process_failure_error;
+}
+
+export function createErrorResponseBody<T>(exception: T): ErrorResponseBody {
+    const errorResponseBody: ErrorResponseBody = {
+        code: ERROR_CODES.processFailureError,
+        message: errorCodeToMessage(ERROR_CODES.processFailureError),
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+    };
+
+    if (exception instanceof BaseError) {
+        errorResponseBody['code'] = exception.code;
+        errorResponseBody['message'] = errorCodeToMessage(
+            errorResponseBody.code,
+        );
+        errorResponseBody['status'] = exception.status;
+    }
+
+    if (exception instanceof ValidationError) {
+        errorResponseBody['data'] = exception.data;
+    }
+
+    return errorResponseBody;
+}
